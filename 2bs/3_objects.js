e92872cce1ded039e91dc8ec3c173b1ed0694579
Subhendu Majhi <140392393+Subhendu23-Coding@users.noreply.github.com>
2025-03-22 10:28:51 +0000

    objects....

--- 2bs/3_objects.js
+++ 2bs/3_objects.js
@@ -1,5 +1,5 @@
 // Singleton
-// Object.create() // => through constructor method
+// Object.create() //  through constructor method
 
 
 // object literals
@@ -7,10 +7,10 @@
 const mySym = Symbol("key1");
 
 const JsUser = {
-    name: "Subhendu",
+    name: "Subho",
     "full name": "Subhendu Majhi",
     [mySym]: "mykey",  // symbol used as key
-    age: 22,
+    age:  23,
     location: "West Bengal",
     email: "subhendu12@gmail.com",
     isLoggedIn: false,
@@ -19,27 +19,26 @@ const JsUser = {
 
 
 // console.log(JsUser.email);
-console.log(JsUser["email"]);  // subhendu12@gmail.com
-console.log(JsUser["full name"]);  // Subhendu Majhi
-console.log(JsUser[mySym]);  // mykey
+// console.log(JsUser["email"]);  // subhendu12@gmail.com
+// console.log(JsUser["full name"]);  // Subhendu Majhi
+// console.log(JsUser[mySym]);  // mykey
 // console.log(typeof(JsUser.mySym));
 
-JsUser.email = "subhendu12@google.com";
-Object.freeze(JsUser);  // Freezes the object
-JsUser.email = "Subhendu@microsoft.com";  // This will not work because the object is frozen
-console.log(JsUser);  // Output: {name: "Subhendu", ... email: "subhendu12@gmail.com", ...}
+// JsUser.email = "subhendu152@google.com";
+// Object.freeze(JsUser);  // Freezes the object
+// JsUser.email = "Subhendu@microsoft.com";  // This will not work because the object is frozen
+// console.log(JsUser);  // Output: {name: "Subhendu", ... email: "subhendu12@gmail.com", ...}
 
 
 JsUser.greeting = function() {
     console.log("Hello Js User");
 }
-
 console.log(JsUser.greeting);  // Prints: [Function: greeting]
 console.log(JsUser.greeting());  // Prints: Hello Js User
 
 
 JsUser.greetingTwo = function() {
-    console.log("Hello Js User,${this.name}");
+    console.log(`Hello Js User, ${this.name}`);
 }
 
 console.log(JsUser.greetingTwo());  // Output: Hello Js User, Subhendu
