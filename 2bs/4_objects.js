4ac818d6f30df53d5260f17600708cac212abc7e
Subhendu Majhi <140392393+Subhendu23-Coding@users.noreply.github.com>
2025-03-22 10:29:58 +0000

    objects2

--- 2bs/4_objects.js
+++ 2bs/4_objects.js
@@ -1,13 +1,17 @@
-// const tinderUser = new Object() // => singleton object
+// const tinderUser = new Object() //  singleton object
 
-const tinderUser = {} // => non-singleton object
+const tinderUser = {} // non-singleton object
 
-tinderUser.id = "105az"
-tinderUser.name = "Subho"
-tinderUser.isLoggdIn = "false"
+// let tinderUser = {}; // Initialize the tinderUser object
 
+// tinderUser.id = "123asd";
+// tinderUser.name = "Subho";
+// tinderUser.isLoggedIn = false; // Boolean value, corrected the typo ("isLoggdIn" to "isLoggedIn")
 
 // console.log(tinderUser);
+
+
+
 
 const regulerUser = {
     email : "subhendu@gmail.com",
@@ -23,15 +27,20 @@ const regulerUser = {
 
 // console.log(regulerUser.fullname.userfullname.firstname);
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 2: "b"}
-
-// const obj3 = {obj1, obj2}
-const obj3 = {...onj1, ...obj2}
-// const obj3 = Object.assign(onj1,obj2, obj4)
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "a", 4: "b"};
+const obj4 = {5: "a", 6: "b"};
 
 
+// console.log(obj1);
+
+const obj3 = Object.assign({}, obj1, obj2, obj4);
 // console.log(obj3);
+
+const obj3Spread = {...obj1, ...obj2, ...obj4};
+// console.log(obj3Spread);
+
+
 
 const users = [
     {
@@ -48,18 +57,19 @@ const users = [
     }
 ]
 
-console.log(user[1].email);
-
-console.log(tinderUser);
-
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(users[1].email);
 
 
+// console.log(tinderUser);
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // => true
-console.log(tinderUser.hasOwnProperty('isLogged'));   // => false
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // => true
+// console.log(tinderUser.hasOwnProperty('isLogged'));   // => false
 
 
 //Destructuring an Object
@@ -70,11 +80,13 @@ const course = {
     courseInstructor: "Subhendu"
 }
 
-// course.courseInstructor
-const {courseInstructor: instructor} = course;
+// console.log(course.courseInstructor);
 
-// console.log(courseInstructor);
-console.log(instructor);
+
+const { courseInstructor: instructor } = course;
+
+// console.log(instructor);
+
 
 // const navbar = ({company}) => {
 
@@ -88,8 +100,8 @@ console.log(instructor);
 //     "price": "free"
 // }
 
-[
-    {},
-    {},
-    {},
-]
+// [
+//     {},
+//     {},
+//     {},
+// ]
