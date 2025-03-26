67acdb8a60055206981c36ac95680cad98d84c7b
Subhendu Majhi <140392393+Subhendu23-Coding@users.noreply.github.com>
2025-03-26 04:42:00 +0000

    Arrow Function..

--- 3bs/3_arrow.js
+++ 3bs/3_arrow.js
@@ -1,58 +1,59 @@
 const user = {
-    username: "Subhendu",
+    username: "Subho",
     price: 999,
 
-    welcomeMessage: function(){
-        console.log('${this.username}, Welcome to the website');
-        console.log(this);
+    welcomeMessage: function() {
+        // console.log(`${this.username}, Welcome to the website`);
+        // console.log(this); // Logs the 'user' object
     }
 };
 
-user.welcomeMessage();
-user.username = 'ram';
-user.welcomeMessage();
+// user.welcomeMessage();
+// user.username = 'ram';
+// user.welcomeMessage();
 
-console.log(`${this.username}, Welcome to the website`);
+// console.log(`${this.username}, Welcome to the website`);
 
-console.log(this) // exprcted output in node : {}
-
-//  Window object is the global object inside the browser or, Inside the browser the global object is window object
-
-// that is why we can capture all the event from window object like click, form submit
+// console.log(this) // exprcted output in node : {}
 
 function chai(){
-    let username = "Subhendu";
+    let username = "Subho";
     console.log(this.username); // expected output: undefined
 }
 
-chai();
+// chai();
 
 const chai = function () {
-    let username = "Subhendu";
+    let username = "Subho";
     console.log(this.username); // expected output: undefined
 }
 
+// chai();
+
 const chai = () => {
-    let username = "Subhendu";
+    let username = "Subho";
     console.log(this); // expected output: {}
     console.log(this.username); // expected output: undefined
 }
 
 // chai();
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2; // explicit return
-// }
+
+const addTwo = (number1, number2) => {
+    return number1 + number2; // explicit return
+}
+
+// console.log(addTwo(3, 4))
 
 // const addTwo = (num1, num2) => num1 + num2; // implicit return
 // const addTwo = (num1, num2) => (num1 + num2); // implicit return
-const addTwo = (num1, num2) => ({username: "hitesh"});
-console.log(addTwo(3, 4));
+// const addTwo = (num1, num2) => ({username: "Subho"});
+// console.log(addTwo(3, 4));
 
-const myArray = [2, 5, 3, 7, 8];
+// const myArray = [2, 5, 3, 7, 8];
 
-myArray.forEach((element) => {
+// myArray.forEach((element) => {
 
-    console.log(element); // prints each element of the array
-});
+//     console.log(element); // prints each element of the array
+// });
 
