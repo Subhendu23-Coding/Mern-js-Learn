4d307380a0211b76ea3dd7a2dd18fb89557b7f41
Subhendu Majhi <140392393+Subhendu23-Coding@users.noreply.github.com>
2025-03-26 04:44:17 +0000

    IIFE

--- 3bs/4_iife.js
+++ 3bs/4_iife.js
@@ -1,52 +1,31 @@
-/******************************Code Execution */
+// Immediately Invoked Function Expressions (IIFE)
 
-// JavaScript is single threaded
 
-// {} Global Execution Context (this)
-// Function or Functional Execution Context
-// Eval Execution Context
+// (function chai(){
+//     console.log(`DB Connected`);
+// });
+// chai();
 
-// first phase
-// *Global Execution* or *Parent Execution Context*
 
-// second phase
-// *Memory Creation Phase* or *Memory Phase* or *Creation Phase*
-// all the value will be declared as 'undefined' before excution phase
+(function chai(){
+    console.log(`DB Connected`);
+    return "Function executed successfully!";
+})();
 
-let val1 = 10;
-let val2 = 15;
-function addNum(num1, num2){
-    let total = num1 + num2;
-    return total;
-}
-// third phase
-// *Executzion Phase* or *Execution Context*
 
-// new execution context (new sandbox)
-// new variable environment + execution thread
-// after execution, new execution context will be deleted and again if (new sandbox) required it will create new execution context again and delete again. and return will be stored in global or parent execution context
+(function code(){
+    console.log('DB CONNECTED NEW')
+})();
 
-let result1 = addNum(val1, val2);
-let result2 = addNum(35, 36);
 
-console.log(result1);
-console.log(result2);
 
-// ****************** Call Stack ******************
+((name) => {
+    console.log(`Db Connection Successful ${name}`);
+})('Subho');
 
-// LIFO => Last In First Out
 
-function one(){
-    console.log("one");
-    two();
-}
-function two(){
-    console.log("two");
-    three();
-}
-function three(){
-    console.log("three");
-}
-one();
-two();
-three();
+
+( (name) => {
+    // Simple IIFE or Unnamed IIFE
+    console.log(`${name} not connected`);
+} )('Subho');
